45170dc5433e850a1920ea4813f0a6fd5c47089c
Ly <1137544897@qq.com>
2020-02-29 00:03:35 +0800

    'add'

--- app/src/main/java/com/ly/wanfun/ui/ac/IndexActivity.kt
+++ app/src/main/java/com/ly/wanfun/ui/ac/IndexActivity.kt
@@ -1,24 +1,17 @@
 package com.ly.wanfun.ui.ac
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.ViewGroup
 import android.widget.TextView
-import android.widget.Toast
 import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
-import com.google.android.material.tabs.TabLayout
-import com.google.android.material.tabs.TabLayoutMediator
 import com.ly.wanfun.R
 import com.ly.wanfun.base.BaseActivity
-import com.ly.wanfun.ui.adapter.index.IndexContainerAdapter
 import com.ly.wanfun.ui.fr.frist.FirstFragment
 import com.ly.wanfun.ui.fr.sec.SecondFragment
 import com.ly.wanfun.ui.fr.thrid.ThridFragment
+import com.ly.wanfun.utils.Utils
 import kotlinx.android.synthetic.main.activity_index.*
 
 class IndexActivity : BaseActivity<IndexVm, ViewDataBinding>() {
@@ -33,42 +26,61 @@ class IndexActivity : BaseActivity<IndexVm, ViewDataBinding>() {
         fragments.add(SecondFragment.newInstance())
         fragments.add(FirstFragment.newInstance())
         fragments.add(ThridFragment.newInstance())
-
         initViewPage()
-
-
     }
 
     /**
      * 初始化viewpager
      */
     private fun initViewPage() {
-        val fragmentMenus = arrayListOf("我的", "文章", "广场")
         vpMainContainer.adapter = object : FragmentStateAdapter(this) {
             override fun getItemCount() = fragments.size
             override fun createFragment(position: Int) = fragments[position]
         }
         vpMainContainer.offscreenPageLimit = fragments.size - 1
-        vpMainContainer.isUserInputEnabled = false
-        TabLayoutMediator(
-            btmNavigationMenu,
-            vpMainContainer,
-            TabLayoutMediator.TabConfigurationStrategy { tab, position ->
-                tab.text = fragmentMenus[position]
-                ((tab.view.getChildAt(1)) as TextView).textSize = 86f
-            }).attach()
+        vpMainContainer.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
+                updateTabMenuStyle(position)
+                super.onPageScrolled(position, positionOffset, positionOffsetPixels)
+            }
+        })
         vpMainContainer.currentItem = 1
-//        btmNavigationMenu
-//        btmNavigationMenu.setOnNavigationItemSelectedListener { it ->
-//            when (it.itemId) {
-//                R.id.firstFragment -> vpMainContainer.currentItem = 0
-//                R.id.secondFragment -> vpMainContainer.currentItem = 1
-//                R.id.thridFragment -> vpMainContainer.currentItem = 2
-//                else -> vpMainContainer.currentItem = 0
-//            }
-//            true
-//        }
+        tvIndexSquare.setOnClickListener { vpMainContainer.currentItem = 2 }
+        tvIndexArticle.setOnClickListener { vpMainContainer.currentItem = 1 }
+        tvIndexMine.setOnClickListener { vpMainContainer.currentItem = 0 }
+    }
 
+    fun updateTabMenuStyle(position: Int) {
+        fun selectEd(tv: TextView) {
+            tv.textSize = Utils.px2dip(this, resources.getDimension(R.dimen.font_20)).toFloat()
+            tv.setTextColor(resources.getColor(R.color.color_18C8A1))
+        }
+
+        fun unSelectEd(tv: TextView) {
+            tv.textSize = Utils.px2dip(this, resources.getDimension(R.dimen.font_18)).toFloat()
+            tv.setTextColor(resources.getColor(R.color.color_48586D))
+        }
+        when (position) {
+            0 -> {
+                selectEd(tvIndexMine)
+                unSelectEd(tvIndexArticle)
+                unSelectEd(tvIndexSquare)
+            }
+            1 -> {
+                unSelectEd(tvIndexMine)
+                selectEd(tvIndexArticle)
+                unSelectEd(tvIndexSquare)
+            }
+            2 -> {
+                unSelectEd(tvIndexMine)
+                unSelectEd(tvIndexArticle)
+                selectEd(tvIndexSquare)
+            }
+        }
     }
 
     override fun initData() {
--- app/src/main/java/com/ly/wanfun/utils/Utils.kt
+++ app/src/main/java/com/ly/wanfun/utils/Utils.kt
@@ -1,8 +1,19 @@
 package com.ly.wanfun.utils
 
 import android.app.Application
+import android.content.Context
 
 object Utils {
 
     fun getApp(): Application = Application()
+
+    fun dip2px(context: Context, dpValue: Float): Int {
+        val scale: Float = context.resources.displayMetrics.density
+        return (dpValue * scale + 0.5f).toInt()
+    }
+
+    fun px2dip(context: Context, pxValue: Float): Int {
+        val scale = context.resources.displayMetrics.density
+        return (pxValue / scale + 0.5f).toInt()
+    }
 }
